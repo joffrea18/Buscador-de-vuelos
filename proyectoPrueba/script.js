49ada31445ccbc25ee4965da78ff9a8507f69491
rto1991 <rubentaibootero@gmail.com>
2023-03-27 02:08:38 +0200

    portadaAnimacion

--- proyectoPrueba/script.js
+++ proyectoPrueba/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// FUNCION SALUDO
+
 function mostrarSaludo(idioma, mensaje) {
   document.getElementById("saludo").innerHTML = mensaje;
   console.log(mensaje);
@@ -21,8 +23,38 @@ setTimeout(function () {
   mostrarSaludo("frances", "Accueillir!");
 }, 8000);
 
-setTimeout(function () {}, 10000);
+setTimeout(function () {
+  mostrarSaludo("español", "FlyWithMe");
+}, 10000);
 
 setTimeout(function () {
-  document.getElementById("saludo").style.display = "none";
-}, 10000);
+  mostrarSaludo("español", "FlyWithMe");
+}, 12000);
+
+// DIA/NOCHE
+
+const buttonDark = document.querySelector("#dark");
+const form = document.querySelector("form");
+const labels = document.querySelectorAll("label.dia");
+const elementsWithDayClass = document.querySelectorAll(".day");
+
+buttonDark.addEventListener("click", () => {
+  form.classList.toggle("day");
+  labels.forEach((label) => {
+    label.classList.toggle("dia");
+  });
+  elementsWithDayClass.forEach((element) => {
+    element.classList.toggle("day");
+  });
+});
+
+// FUNCION PARA CAMBIAR LA CLASE DAY EN EL FORM
+
+buttonDark.addEventListener("click", () => {
+  form.classList.toggle("day");
+  if (form.classList.contains("day")) {
+    body.style.backgroundColor = "#fff";
+  } else {
+    body.style.backgroundColor = "#f6f6f6";
+  }
+});
