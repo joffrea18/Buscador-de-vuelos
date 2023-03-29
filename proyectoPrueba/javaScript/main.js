32b703850413c8032b06563befda0ca062edcb82
rto1991 <rubentaibootero@gmail.com>
2023-03-29 02:46:00 +0200

    avancesPresentacion

---
+++ proyectoPrueba/javaScript/main.js
@@ -0,0 +1,26 @@
+// API Key
+// 4GtAHlDeMysVlq06H7iz7Z7IVcw6rEpZ
+// API Secret
+// lq4gnxLYfwNZrGAt
+
+"use strict";
+
+// const apiKey = "4GtAHlDeMysVlq06H7iz7Z7IVcw6rEpZ";
+// const apiSecret = "lq4gnxLYfwNZrGAt";
+// let token;
+// let tokenRequest;
+
+// export async function tokenLlamada ()
+//     let respuesta = fletch(
+//         "https://test.api.amadeus.com/v1/security/oauth2/token",
+//     {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/x-www-form-urlencoded",
+//         },
+//         body: `grant_type=client_credentials&client_id=${apiKey}&client_secret=${apiSecret}`,
+//     }
+//     );
+
+//     let data = (await respuesta).json();
+//     return data
