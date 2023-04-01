252aab3b81cf654ba3a9d8238f3ef65a5c41189b
JoffreArias <123706095+joffrea18@users.noreply.github.com>
2023-04-01 12:55:50 +0200

    Delete x.js

--- js/x.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-let url = `https://test.api.amadeus.com/v1/reference-data/locations`;
-fetch(url)
-	.then((response) => response.json())
-	.then((data) => console.log(data))
-	.catch((error) => console.log(error));
